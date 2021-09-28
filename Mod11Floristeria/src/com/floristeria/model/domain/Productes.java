e6d95ac065942feaeae21aef32633dff3a13b874
Joel Cabatay <joel@fedora>
2021-09-28 11:06:56 +0200

    Creació Clase Productes

---
+++ Mod11Floristeria/src/com/floristeria/model/domain/Productes.java
@@ -0,0 +1,19 @@
+package com.floristeria.model.domain;
+
+public class Productes {
+	
+	private double preu;
+	
+	public Productes(double preu) {
+		this.preu = preu;
+	}
+
+	public double getPreu() {
+		return preu;
+	}
+
+	public void setPreu(double preu) {
+		this.preu = preu;
+	}
+
+}
