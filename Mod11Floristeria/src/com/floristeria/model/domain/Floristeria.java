d6726d962a7d24ff0f3f761ced42139f7f109ffc
Joel Cabatay <joel@fedora>
2021-09-28 11:01:54 +0200

    Creació clase Floristeria

---
+++ Mod11Floristeria/src/com/floristeria/model/domain/Floristeria.java
@@ -0,0 +1,11 @@
+package com.floristeria.model.domain;
+
+public class Floristeria {
+	private String nom;
+	
+	public Floristeria(String nom) {
+		this.nom = nom;
+	}
+	
+
+}
---
+++ Mod11Floristeria/src/com/floristeria/model/view/Main.java
@@ -0,0 +1,9 @@
+package com.floristeria.model.view;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
