e1f83bda116d24fe33c26d4eceda0cac2dd62ee3
Joel Cabatay <joel@fedora>
2021-09-28 11:19:20 +0200

    Creació plantilla ProductesController

---
+++ Mod11Floristeria/src/com/floristeria/controller/ProductesController.java
@@ -0,0 +1,12 @@
+package com.floristeria.controller;
+
+public class ProductesController {
+	/*
+	 * - Aquí crearem les instancies:
+	 * - afegirArbre
+	 * - afegirFlor
+	 * - afegirDecoracio
+	 * - mostrarDadesProductes (arbres, flores i decoració)
+	 */
+
+}
